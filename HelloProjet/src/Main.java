5cf04b05667ec1ab8a950c50c1d3ef85ac0dd6ad
liuyz <liuyz@126.com>
2020-08-05 08:09:48 +0800

    下载以后修改的4

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-
+        System.out.println(6);
 
     }
 }
---
+++ HelloProjet/src/Sdd.java
@@ -0,0 +1,13 @@
+/**
+ * <一句话功能简述><br>
+ * <>
+ *
+ * @author $ {USER}
+ * @create $ {DATE}
+ * @since 1.0.0
+ */
+
+public class Sdd {
+
+
+}
